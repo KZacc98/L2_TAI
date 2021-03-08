8acd1f12c54e8d99ed185eaa30beecb7103faaf6
Kamil Zachara <kamil.zachara98@gmail.com>
2021-03-08 09:18:38 +0100

    script done... kinda', not sure doe

---
+++ navbar.js
@@ -0,0 +1,11 @@
+let nav = '<nav><a href="index.html">Home</a>'+
+ '<a href="kontakt.html">Kontakt</a> </nav>';
+
+
+let elem = document.getElementById("identyfikator");
+let elem2 = document.getElementsByTagName("tag");
+let elem3 = document.getElementsByClassName("class"); 
+let elem4 = document.querySelector(".klasa");
+let elems = document.querySelectorAll("a.link");
+
+elem.innerHTML='<div>test</div>'
